4e62d616e23ccfa16c93a7601535c531a5dc2d52
Jérôme FOURMOND <jerome@fourmond.fr>
2017-02-16 12:27:27 +0100

    Updated README

---
+++ EyeEvolution/scripts/script.R
@@ -0,0 +1,40 @@
+csvDir <- "../resources/";
+
+# Récupération du nom des fichiers csv dans le répertoire "resources"
+print('Récupération du nom des fichiers csv dans le répertoire "resources')
+csvFiles <- list.files(path=csvDir, pattern="*.csv")
+
+# Concanténation du nom répertoire comme préfixe au nom des fichiers
+for(i in 1:length(csvFiles)) {
+  csvFiles[i] <- paste(csvDir, csvFiles[i], sep="", collapse=NULL)
+}
+
+# Lecture des fichiers dans une liste de dataframes
+print('Lecture des fichiers dans une liste de dataframes')
+datas <- lapply(csvFiles, read.csv, h=T, sep=";")
+
+# Concaténation en un seul gros dataframe
+print('Concaténation en un seul gros dataframe')
+data <- do.call(rbind, datas)
+
+# Suppression de la colonne X, inconnue au bataillon
+print('Suppression de la colonne X')
+data$X <- NULL
+
+# Plot 1
+print('Plot 1')
+plot(data$cross.over.rate, data$best.eye.fitness, pch=3, xlab="crossover rate", ylab="best eye fitness")
+summary(lm(data$best.eye.fitness ~ data$cross.over.rate))
+abline(lm(data$best.eye.fitness ~ data$cross.over.rate))
+
+# Plot 2
+print('Plot 2')
+plot(data$mutation.rate, data$best.eye.fitness, pch=3, xlab="mutation rate", ylab="best eye fitness")
+summary(lm(data$best.eye.fitness ~ data$mutation.rate))
+abline(lm(data$best.eye.fitness ~ data$mutation.rate))
+
+# Plot 3
+print('Plot 3')
+plot(data$population.size, data$best.eye.fitness, pch=3, xlab="population size", ylab="best eye fitness")
+summary(lm(data$best.eye.fitness ~ data$population.size))
+abline(lm(data$best.eye.fitness ~ data$population.size))
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# EyeEvolution
+# Eye Evolution
+
+## Objectif
+
+L'objectif est de programmer un algorithme génétique permettant de faire évoluer une population d'"individus", où chaque "individu" code les paramètres d'un oeil, d'après la modélisation suivante :
+
+![Modélisation](/Rapport/modelisation.png)
+
