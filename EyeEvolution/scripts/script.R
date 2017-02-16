b1d8e2b571908734328df498df55c928ab0b3753
Jérôme FOURMOND <jerome@fourmond.fr>
2017-02-16 13:14:40 +0100

    Updated R Script

--- EyeEvolution/scripts/script.R
+++ EyeEvolution/scripts/script.R
@@ -1,7 +1,7 @@
 csvDir <- "../resources/";
 
 # Récupération du nom des fichiers csv dans le répertoire "resources"
-print('Récupération du nom des fichiers csv dans le répertoire "resources')
+print('Récupération du nom des fichiers csv dans le répertoire "resources"')
 csvFiles <- list.files(path=csvDir, pattern="*.csv")
 
 # Concanténation du nom répertoire comme préfixe au nom des fichiers
@@ -23,18 +23,24 @@ data$X <- NULL
 
 # Plot 1
 print('Plot 1')
+jpeg(file="plot_crossoverrate_fitness.jpeg")
 plot(data$cross.over.rate, data$best.eye.fitness, pch=3, xlab="crossover rate", ylab="best eye fitness")
 summary(lm(data$best.eye.fitness ~ data$cross.over.rate))
 abline(lm(data$best.eye.fitness ~ data$cross.over.rate))
+dev.off()
 
 # Plot 2
 print('Plot 2')
+jpeg(file="plot_mutationrate_fitness.jpeg")
 plot(data$mutation.rate, data$best.eye.fitness, pch=3, xlab="mutation rate", ylab="best eye fitness")
 summary(lm(data$best.eye.fitness ~ data$mutation.rate))
 abline(lm(data$best.eye.fitness ~ data$mutation.rate))
+dev.off()
 
 # Plot 3
 print('Plot 3')
+jpeg(file="plot_populationsize_fitness.jpeg")
 plot(data$population.size, data$best.eye.fitness, pch=3, xlab="population size", ylab="best eye fitness")
 summary(lm(data$best.eye.fitness ~ data$population.size))
 abline(lm(data$best.eye.fitness ~ data$population.size))
+dev.off()
